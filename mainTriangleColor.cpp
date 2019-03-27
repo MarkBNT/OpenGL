856f7ce45d34963bf1b410653846cb087901d353
MarkBNT <mark.bertran@E120-07>
2019-03-27 21:04:24 +0100

    Se han añadido cosas que hacer para ir trabajando ademas de poner nuevas cosas en el GITIGNORE

---
+++ mainTriangleColor.cpp
@@ -0,0 +1,189 @@
+//Using SDL, SDL OpenGL, GLEW, standard IO, and strings
+#include <SDL.h>
+#include "gl\glew.h"
+#include "SDL_opengl.h"
+
+#include <iostream>
+#include <fstream>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Starts up SDL, creates window, and initializes OpenGL
+bool init();
+
+//Initializes rendering program and clear color
+void initGL();
+
+//Per frame update
+void update();
+
+//Renders quad to the screen
+void render();
+
+//Frees media and shuts down SDL
+void close();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//OpenGL context
+SDL_GLContext gContext;
+
+// TO DO : Declare IDs for a VAO and a VBO
+
+// TO DO : Declare Shader variables
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Use OpenGL 3.1 core
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Create context
+			gContext = SDL_GL_CreateContext(gWindow);
+			if (gContext == NULL)
+			{
+				printf("OpenGL context could not be created! SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else
+			{
+				//Initialize GLEW
+				glewExperimental = GL_TRUE;
+				GLenum glewError = glewInit();
+				if (glewError != GLEW_OK)
+				{
+					printf("Error initializing GLEW! %s\n", glewGetErrorString(glewError));
+				}
+
+				//Use Vsync
+				if (SDL_GL_SetSwapInterval(1) < 0)
+				{
+					printf("Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError());
+				}
+
+				initGL();
+
+			}
+		}
+	}
+
+	return success;
+}
+
+void initGL()
+{
+	// TO DO : Declare shaders code (Vertex and Fragment) use char* or strings
+
+	// TO DO : Declare vertex data
+
+	// TO DO : Initialize clear color
+
+	// TO DO : Create Vertex Shader, compile and check for errors
+
+	// TO DO : Create Fragment Shader, compile and check for errors
+
+	// TO DO : Link Shaders on a Shader Program and check for errors
+
+	// TO DO : Delete Shader objects (fragment and Vertex).
+
+	// TO DO : Generate VAO and inside it VBO. Enable Attribute and UnBind VAO after process
+
+}
+
+void update()
+{
+	//No per frame update needed
+}
+
+void render()
+{
+	//TO DO : Clear color buffer
+
+	// TO DO : Use Shader, Bind VAO and Paint
+
+	// TO DO : UnBind VAO and Shader Program
+
+}
+
+void close()
+{
+	//TO DO : Clear data in GPU
+
+	//Destroy window	
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+	//Start up SDL and create window
+	if (!init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		//Main loop flag
+		bool quit = false;
+
+		//Event handler
+		SDL_Event e;
+
+		//While application is running
+		while (!quit)
+		{
+			//Handle events on queue
+			while (SDL_PollEvent(&e) != 0)
+			{
+				if (e.type == SDL_KEYDOWN) {
+					if (e.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
+						quit = true;
+					}
+				}
+				//User requests quit
+				if (e.type == SDL_QUIT)
+				{
+					quit = true;
+				}
+			}
+
+			//Render quad
+			render();
+
+			//Update screen
+			SDL_GL_SwapWindow(gWindow);
+		}
+	}
+
+	//Free resources and close SDL
+	close();
+
+	return 0;
+}
